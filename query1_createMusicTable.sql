af07152876835e137e3d09b774e5eed09ba7054a
John Alexis Guerra Gómez <john.guerra@gmail.com>
2024-02-13 15:23:46 -0800

    Adding query1

---
+++ query1_createMusicTable.sql
@@ -0,0 +1,5 @@
+-- Query 1: Create a new Table Music Video, that is a class of type Track (generalization) but adds the attribute Video director. A music video is a track. There cannot be a video without a track, and each track can have either no video or just one. 
+-- Author: John Alexis Guerra Gomez 
+
+
+SELECT * FROM MusicVideo;
